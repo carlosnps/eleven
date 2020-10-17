c793b73fb66a7b5cb7321451edac8afc1a80e92e
Your Name <carlosecoto@gmail.com>
2020-10-17 15:23:53 -0400

    Delete and redo all

--- server.js
+++ server.js
@@ -19,25 +19,64 @@ app.use('/notes', notes);
 app.get('/api/notes', function(req, res){
      res.sendfile('./db/db.json');
 });
-//app.use('/apinotes', apis);
-
-app.post('/apinotes',function(req,res){
-  var title=req.body.title;
-  var text=req.body.text;
-//  var title = 'carlos';
-//  var text = 'coto';
-  console.log("Title = "+title+", Text is "+text);
+//app.('/apinotes', apis);
+app.get('/list' , function(req, res){
   fs.readFile('./db/db.json', 'utf8', function readFileCallback(err, data){
     if (err){
         console.log(err);
     } else {
-    obj = JSON.parse(data); //now it an object
-    obj.push({title: title, text: text}); //add some data
-    json = JSON.stringify(obj); //convert it back to json
-    fs.writeFile('./db/db.json' , json, 'utf8', function(err){
-      if(err) return console.log(err);
-      console.log('Note added');
-    });
+      var respond = '';
+      obj = JSON.parse(data);
+      console.log("data length is " + obj.length);
+      for(var i = 0; i < obj.length; i++) {
+        respond = respond + "key " + i + " " + obj[i].title + " " + obj[i].text + "<\p>";
+        console.log(obj[i]);
+      }
+      res.send(respond);
+    }
+  });
+});
+
+app.delete('/api/notes/:id' , function (req, res) {
+  var id = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+      obj = JSON.parse(data);
+      if (id in obj) {
+        var oldtitle = obj[id].title;
+        var oldtext = obj[id].text;
+        obj.splice(id, 1);
+        json = JSON.stringify(obj); //convert it back to json
+        fs.writeFile('./db/db.json' , json, 'utf8', function(err){
+          if(err) return res.send("unable to delete json record");
+          res.send("deleted " + oldtitle + " " + oldtext);
+        });
+
+      } else {
+        res.send("key does not exist " + id);
+      }
+    }
+  });
+});
+
+app.post('/apinotes',function(req,res){
+  var title=req.body.title;
+  var text=req.body.text;
+  console.log("Title = "+title+", Text is "+text);
+  fs.readFile('./db/db.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        return res.send(err);
+    } else {
+      obj = JSON.parse(data); //now it an object
+      console.log("object is " + obj);
+      obj.push({title: title, text: text}); //add some data
+      json = JSON.stringify(obj); //convert it back to json
+      fs.writeFile('./db/db.json' , json, 'utf8', function(err){
+        if(err) console.log("error in writing");
+        res.send("Note has been added");
+      });
   }});
 });
 
