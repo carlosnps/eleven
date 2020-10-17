ea3644b0d7ba543a53af327485f8b84946592a9b
Your Name <carlosecoto@gmail.com>
2020-10-16 22:08:30 -0400

    posting after text and title

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-const express = require("express"),
-  bodyParser = require('body-parser');
-//any other required goes here
+const express = require("express");
 
+//any other required goes here
 // Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 var notes = require('./routes/htmlRoutes');
+var apis = require('./routes/apiRoutes');
 var apinotes = require('./db/db.json');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 app.use('/notes', notes);
 app.get('/api/notes', function(req, res){
      res.send(apinotes);
 });
+//app.use('/apinotes', apis);
 
-app.post('apinotes',function(req,res){
+app.post('/apinotes',function(req,res){
   var title=req.body.title;
   var text=req.body.text;
+//  var title = 'carlos';
+//  var text = 'coto';
   console.log("Title = "+title+", Text is "+text);
   res.end("yes");
 });
